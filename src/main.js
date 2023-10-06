2aa7965b5f52d56329e5ab6efbf9ac4ee2476813
Jaewoong <dixk3458@naver.com>
2023-10-06 20:49:44 +0900

    JS-게임타이머

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ const ITEM_SIZE = 80;
 const WATER_COUNT = 5;
 const BRANCH_COUNT = 5;
 
+const GAME_DURATION_SEC = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 
@@ -14,7 +16,7 @@ const gameScore = document.querySelector('.game__score');
 
 let started = false;
 let score = BRANCH_COUNT;
-let time = undefined;
+let timer = undefined;
 
 gameBtn.addEventListener('click', () => {
   if (started) {
@@ -30,6 +32,24 @@ function startGame() {
   initGame();
   showTimerAndScore();
   startGameTimer();
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  gameTimer.innerText = `${minutes}:${seconds}`;
 }
 
 function showTimerAndScore() {
