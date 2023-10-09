d75822af7642403f7fb55fe978860ae3e6de277c
Jaewoong <dixk3458@naver.com>
2023-10-09 23:08:35 +0900

    Refactoring - 함수중복제거

--- src/game.js
+++ src/game.js
@@ -51,7 +51,7 @@ class Game {
 
     this.gameBtn.addEventListener('click', () => {
       if (this.started) {
-        this.stop();
+        this.stop(Reason.cancel);
       } else {
         this.start();
       }
@@ -69,10 +69,10 @@ class Game {
       this.score++;
       this.updateScoreBoard(this.score);
       if (this.score === this.BRANCH_COUNT) {
-        this.finish(true);
+        this.stop(Reason.win);
       }
     } else if (item === 'water') {
-      this.finish(false);
+      this.stop(Reason.lose);
     }
   };
 
@@ -90,26 +90,12 @@ class Game {
     sound.playBackground();
   }
 
-  stop() {
+  stop(reason) {
     this.started = false;
     this.stopGameTimer();
     this.hideGameButton();
-    this.onGameStop && this.onGameStop(Reason.cancel);
-    sound.playAlert();
     sound.stopBackground();
-  }
-
-  finish(win) {
-    this.started = false;
-    this.hideGameButton();
-    if (win) {
-      sound.playWin();
-    } else {
-      sound.playWater();
-    }
-    this.stopGameTimer();
-    sound.stopBackground();
-    this.onGameStop && this.onGameStop(win ? Reason.win : Reason.lose);
+    this.onGameStop && this.onGameStop(reason);
   }
 
   initGame() {
@@ -136,7 +122,7 @@ class Game {
     this.timer = setInterval(() => {
       if (remainingTimeSec <= 0) {
         clearInterval(timer);
-        this.finish(score === this.BRANCH_COUNT);
+        this.stop(score === this.BRANCH_COUNT ? Reason.win : Reason.lose);
         return;
       }
       this.updateTimerText(--remainingTimeSec);
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 import { GameBuilder, Reason } from './game.js';
 import PopUp from './popup.js';
+import * as sound from './sound.js';
 
 const game = new GameBuilder()
   .withGameDuration(10)
@@ -16,12 +17,15 @@ game.setGameStopListener(reason => {
   switch (reason) {
     case Reason.cancel:
       message = 'REPLAY ❓';
+      sound.playAlert();
       break;
     case Reason.win:
       message = 'YOU WIN 🎉';
+      sound.playWin();
       break;
     case Reason.lose:
       message = 'YOU LOST 😜';
+      sound.playWater();
       break;
     default:
       throw new Error('not valid reason');
